d8052da7753272a4e662de9d3fccbf3ece14100f
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-12-19 21:54:31 +0100

    Update Kingdom-Animalia.js

--- Kingdom-Animalia.js
+++ Kingdom-Animalia.js
@@ -1,3 +1,12 @@
-class Animalia{
-    
+class Backbone{
+    Backbone(){
+        console.log("Has Backbone");
+    } 
 }
+
+class NoBackbone{
+    Backbone(){
+        console.log("Has No Backbone");
+    } 
+}
+
