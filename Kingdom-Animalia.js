1887f0bedf31b03ea9af39fd7bc6f33fdb23773b
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-12-19 23:07:22 +0100

    Update Kingdom-Animalia.js

--- Kingdom-Animalia.js
+++ Kingdom-Animalia.js
@@ -1,12 +1,51 @@
-class Backbone{
-    Backbone(){
-        console.log("Has Backbone");
-    } 
+class Animalia{
+    constructor(){
+        if(this.constructor === Animalia){
+            throw new Error("Animalia cannot be instantiated")
+        }
+    }
+
+    HasBackBone(){
+        throw new Error("This method cannot be called")  
+    };
+    ColdBlood(){
+        throw new Error("This method cannot be called")  
+    };
+    WarmBlood(){
+        throw new Error("This method cannot be called")  
+    };
+
+    
 }
 
-class NoBackbone{
-    Backbone(){
-        console.log("Has No Backbone");
-    } 
+class Arthropoda{
+    HasBackBone(){
+        return "Arthropoda has no BackBone";
+    }
+}
+class Fish{
+    HasBackBone(){
+        return "Fish has BackBone";
+    }
+}
+class Amphibia{
+    HasBackBone(){
+        return "Amphibia has BackBone";
+    }
+}
+class Reptiles{
+    HasBackBone(){
+        return "Reptiles has BackBone";
+    }
+}
+class AVES{
+    HasBackBone(){
+        return "AVES has BackBone";
+    }
 }
 
+class Mammals{
+    HasBackBone(){
+        return "Mammals has BackBone";
+    } 
+}
