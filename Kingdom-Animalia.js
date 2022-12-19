28988a5ef660c89c035caf82f71cb81f42b22be8
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-12-19 23:23:26 +0100

    Update Kingdom-Animalia.js

--- Kingdom-Animalia.js
+++ Kingdom-Animalia.js
@@ -4,48 +4,82 @@ class Animalia{
             throw new Error("Animalia cannot be instantiated")
         }
     }
-
+    #Intro(){
+        console.log("Welcome to Animalia")
+    }; 
     HasBackBone(){
-        throw new Error("This method cannot be called")  
+        throw new Error("This method cannot be called by this instance")  
     };
     ColdBlood(){
-        throw new Error("This method cannot be called")  
+        throw new Error("This method cannot be called by this instance")  
     };
     WarmBlood(){
-        throw new Error("This method cannot be called")  
+        throw new Error("This method cannot be called by this instance")  
+    };
+    Greetings(){
+        this.#Intro()
     };
 
     
 }
 
-class Arthropoda{
+
+class Arthropoda extends Animalia{
     HasBackBone(){
         return "Arthropoda has no BackBone";
     }
-}
-class Fish{
-    HasBackBone(){
-        return "Fish has BackBone";
-    }
-}
-class Amphibia{
-    HasBackBone(){
-        return "Amphibia has BackBone";
-    }
-}
-class Reptiles{
-    HasBackBone(){
-        return "Reptiles has BackBone";
-    }
-}
-class AVES{
-    HasBackBone(){
-        return "AVES has BackBone";
+    ColdBlood(){
+        return "Arthropoda are ColdBlooded";
     }
 }
 
-class Mammals{
+
+class Fish extends Animalia{
+    HasBackBone(){
+        return "Fish has BackBone";
+    }
+    ColdBlood(){
+        return "Fish are ColdBlooded"
+    }
+}
+
+
+class Amphibia extends Animalia{
+    HasBackBone(){
+        return "Amphibia has BackBone";
+    }
+    ColdBlood(){
+        return "Amphibia are ColdBlooded"
+    }
+}
+
+
+class Reptiles extends Animalia{
+    HasBackBone(){
+        return "Reptiles has BackBone";
+    }
+    ColdBlood(){
+        return "Reptiles are ColdBlooded"
+    }
+}
+class AVES extends Animalia{
+    HasBackBone(){
+        return "AVES has BackBone";
+    }
+    WarmBlood(){
+        return "AVES is WarmBlooded"
+    }
+}
+
+class Mammals extends Animalia{
     HasBackBone(){
         return "Mammals has BackBone";
     } 
+    WarmBlood(){
+        return "Mammals is WarmBlooded"
+    }
 }
+
+
+Arthropoda.Greetings();
+Arthropoda.WarmBlood();
